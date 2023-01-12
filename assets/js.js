760bb638515f5aee560c40fba1bc9afb353085cd
Jacob Studstill <jacob.studstill@gmail.com>
2023-01-11 23:38:10 -0500

    Added extra columns and linked api to javascript

---
+++ assets/js.js
@@ -0,0 +1,38 @@
+var getStateBtn = document.getElementById('city-button')
+var cityName = document.getElementById('city-name')
+
+
+function getState(){
+    var cityEquals = document.getElementById('city-input').value
+    console.log(cityEquals);
+    var cityUrl = 'https://api.openweathermap.org/data/2.5/weather?appid=575d8b41f8a1ee499e2844be6f9a6202&units=imperial&q=' + cityEquals;
+    cityName.textContent = cityEquals
+
+    fetch(cityUrl).then(function (response) {
+            return response.json();
+        })
+        .then(function (data){
+            console.log(data.main.temp)
+        })
+
+
+    
+}
+
+// function fetchCurrentWeather(){
+    
+//     fetch(cityUrl).then(function (data) {
+//         if(response.ok) {
+//             console.log(data.main.temp)
+//         }
+//     })
+// }
+
+
+
+
+
+
+
+var getStateBtn = addEventListener("click", getState)
+// fetchCurrentWeather()
